db0360faa9eb174ea2bb5c325319e6cd4ecb7e61
Allen Baker <althebaker43@gmail.com>
2014-06-30 07:40:24 -0700

    Copied main sources from Trinket sample to Launchpad sample.

--- samples/launchpad/launchpad_sample.c
+++ samples/launchpad/launchpad_sample.c
@@ -1,7 +1,59 @@
-#include "TargetSystem.h"
+#include <stdint.h>
 
+#include "TargetSystem.h"
+#include "TimerDriver.h"
+
+static void ToggleLED();
+
+static uint8_t events [SYSTEM_NUM_EVENTS] = {FALSE};
 
 int main()
 {
+  // Initialize timer driver
+  InitTimers();
+
+  TimerInstance* timer = CreateTimer();
+  SetTimerCycleTimeMilliSec(
+      timer,
+      500
+      );
+  SetTimerCycleHandler(
+      timer,
+      ToggleLED
+      );
+  StartTimer(timer);
+
+  System_EventType eventIter = 0;
+  System_EventCallback currentEventCallback = NULL;
+  
+  while(1)
+  {
+    for(
+        eventIter = 0;
+        eventIter < SYSTEM_NUM_EVENTS;
+        ++eventIter
+       )
+    {
+      if (events[eventIter] == FALSE)
+      {
+        continue;
+      }
+
+      currentEventCallback = System_GetEventCallback(eventIter);
+
+      if (currentEventCallback != NULL)
+      {
+        (*currentEventCallback)(eventIter);
+      }
+
+      events[eventIter] = FALSE;
+    }
+  }
+
   return 0;
+}
+
+void
+ToggleLED()
+{
 }
